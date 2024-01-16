f402f01b6867cfaa1ed8b93b8dd8df6b8ba8075f
Julian Meinking <12785972+DrJume@users.noreply.github.com>
2024-01-16 16:28:36 +0100

    build(deps): upgrade deps

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,47 +1,51 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import mdx from "@astrojs/mdx";
-const BASE_PATH = 'lora-meshtastic';
-
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+const BASE_PATH = 'lora-meshtastic'
 
 // https://astro.build/config
 export default defineConfig({
   base: BASE_PATH,
   site: `https://htwdd-rn.github.io/${BASE_PATH}/`,
   redirects: {
-    '/': `/${BASE_PATH}/einfuehrung/projektbeschreibung/`
+    '/': `/${BASE_PATH}/einfuehrung/projektbeschreibung/`,
   },
   trailingSlash: 'always',
-  integrations: [starlight({
-    locales: {
-      root: {
-        lang: 'de-DE',
-        label: 'Deutsch'
-      }
-    },
-    title: 'LoRa Meshtastic',
-    editLink: {
-      baseUrl: 'https://github.com/HTWDD-RN/lora-meshtastic/edit/main/docs/'
-    },
-    social: {
-      github: 'https://github.com/HTWDD-RN/lora-meshtastic'
-    },
-    lastUpdated: true,
-    sidebar: [{
-      label: 'Einführung',
-      autogenerate: {
-        directory: 'einfuehrung'
-      }
-    }, {
-      label: 'FAQ',
-      autogenerate: {
-        directory: 'faq'
-      }
-    }, {
-      label: 'Ergebnisse',
-      autogenerate: {
-        directory: 'ergebnisse'
-      }
-    }]
-  }), mdx()]
-});
+  integrations: [
+    starlight({
+      locales: {
+        root: {
+          lang: 'de-DE',
+          label: 'Deutsch',
+        },
+      },
+      title: 'LoRa Meshtastic',
+      editLink: {
+        baseUrl: 'https://github.com/HTWDD-RN/lora-meshtastic/edit/main/docs/',
+      },
+      social: {
+        github: 'https://github.com/HTWDD-RN/lora-meshtastic',
+      },
+      lastUpdated: true,
+      sidebar: [
+        {
+          label: 'Einführung',
+          autogenerate: {
+            directory: 'einfuehrung',
+          },
+        },
+        {
+          label: 'FAQ',
+          autogenerate: {
+            directory: 'faq',
+          },
+        },
+        {
+          label: 'Ergebnisse',
+          autogenerate: {
+            directory: 'ergebnisse',
+          },
+        },
+      ],
+    }),
+  ],
+})
